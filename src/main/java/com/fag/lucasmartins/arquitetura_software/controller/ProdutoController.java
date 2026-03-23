c5aa8f0af9646ae6130352a7afdd10bb645bd347
Douglas Ribowski <douglasribowski17@gmail.com>
2026-03-23 19:43:28 -0300

    Refatoração do ProdutoController

--- src/main/java/com/fag/lucasmartins/arquitetura_software/controller/ProdutoController.java
+++ src/main/java/com/fag/lucasmartins/arquitetura_software/controller/ProdutoController.java
@@ -1,10 +1,15 @@
 package com.fag.lucasmartins.arquitetura_software.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.fag.lucasmartins.arquitetura_software.model.bo.entity.Produto;
 import com.fag.lucasmartins.arquitetura_software.model.bo.repository.ProdutoRepository;
@@ -49,7 +54,7 @@ public class ProdutoController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                                 .body("Erro interno: " + e.getMessage());
+                                .body("Erro interno: " + e.getMessage());
         }
     }
 }
